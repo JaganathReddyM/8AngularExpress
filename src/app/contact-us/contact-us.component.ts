c70708326894b89b57b4ccb3dcb8d0db6f4b820a
Jaganath Reddy <145546061+JaganathReddyM@users.noreply.github.com>
2023-10-25 10:26:18 +0530

    Update

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent {
-
+  title = 'angularexpress';
+  userList : any =[];
+  data : any;
+  expresponse : string = " ";
+  constructor(private http : HttpClient, private baccess : BackendAccessService ){
+  }
+  getAllUsers(){
+    this.userList = this.baccess.getAllUsers();
+  }
+ 
+  addUser(udata : any){
+    this.expresponse = this.baccess.addUser(udata);
+   
+  }
 }
